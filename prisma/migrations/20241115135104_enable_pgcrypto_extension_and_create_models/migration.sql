24e0dc603a562ed509991af61699cbfe155a9aa9
Leonardo Guedez <leonardoguedezsuazo@gmail.com>
2024-11-15 14:52:08 +0100

    split schema into multiple files

--- prisma/migrations/20241115135104_enable_pgcrypto_extension_and_create_models/migration.sql
+++ prisma/migrations/20241115135104_enable_pgcrypto_extension_and_create_models/migration.sql
@@ -1,6 +1,14 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- CreateTable
+CREATE TABLE "follows" (
+    "follower_id" UUID NOT NULL,
+    "followee_id" UUID NOT NULL,
+
+    CONSTRAINT "follows_pkey" PRIMARY KEY ("follower_id","followee_id")
+);
+
+-- CreateTable
 CREATE TABLE "peeps" (
     "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
     "content" TEXT NOT NULL,
@@ -19,14 +27,6 @@ CREATE TABLE "users" (
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "follows" (
-    "follower_id" UUID NOT NULL,
-    "followee_id" UUID NOT NULL,
-
-    CONSTRAINT "follows_pkey" PRIMARY KEY ("follower_id","followee_id")
 );
 
 -- AddForeignKey
