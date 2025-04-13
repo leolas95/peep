d85bee82ff10906687e3b1a3e0dc05fd9b6c49af
Leonardo Guedez <leonardoguedezsuazo@gmail.com>
2025-04-13 13:21:11 +0200

    fix tests

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../modules/prisma/prisma.service';
+import { PeepService } from '../peep/peep.service';
+import { AuthModule } from './auth.module';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [AuthModule],
       controllers: [AuthController],
+      providers: [
+        AuthService,
+        JwtService,
+        UserService,
+        PrismaService,
+        PeepService,
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaModule } from '../modules/prisma/prisma.module';
+import { PeepModule } from '../peep/peep.module';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [PrismaModule, PeepModule],
+      providers: [AuthService, UserService, JwtService],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
